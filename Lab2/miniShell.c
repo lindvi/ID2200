6d2b215d974a901237df2e9c499694f3602205a6
lindvi <lindvi@kth.se>
2013-05-13 16:34:09 +0200

    wat

--- Lab2/miniShell.c
+++ Lab2/miniShell.c
@@ -37,11 +37,7 @@ void printError(char* command) {
 
 bool checkIfBG(char *input) {
 	int i;
-<<<<<<< HEAD
-	for( i=0; input[i] != NULL; i++);
-=======
 	for( i=0; input[i] != '\0'; i++);
->>>>>>> 441bfe414a5a7126d28b471d2a7059a5e04ee953
 	printf("BG, näst sista: %c\n", input[i-1] );
 	if(input[i-1] == '&')
 		return true;
@@ -77,53 +73,57 @@ int main(int argc, char const *argv[], char *envp[]) {
 		input = (char *)malloc(inBuffer+1);
 		if((bytesRead = *fgets(input, inBuffer, stdin)) < 0)
 			printf("ERROR ERROR ERROR!\n");
-		command = strtok(input, " \n");
+		
 
-		if(strcmp(command, "exit") == 0) {
-			printf("Exiting..\n");
-			running = false;
-		} else if(strcmp(command, "cd") == 0) {
-			command = strtok(NULL, " \n");
-			if(chdir(command) < 0) {
-				strcpy(path, getenv("HOME"));
-				setenv("PWD", path, 1);
-				if(chdir(path) < 0)	
-					printError("cd");
-			} else {
-				strcpy(path, getcwd(0,0));
-				setenv("PWD", path, 1);
-			}
-		} else if(strcmp(command, "") == 0){
+			command = strtok(input, " \n");
 
-		}else {
-			child_pid = fork();
-			if(child_pid == 0) {
-				// CHILD
-				parameters[0] = command;
-				for(i=1; i<6; i++) {
-					temp = strtok(NULL, " \n");
-					parameters[i] = temp;
+			if(command == NULL)
+				command = "";
+			if(strcmp(command, "exit") == 0) {
+				printf("Exiting..\n");
+				running = false;
+			} else if(strcmp(command, "cd") == 0) {
+				command = strtok(NULL, " \n");
+				if(chdir(command) < 0) {
+					strcpy(path, getenv("HOME"));
+					setenv("PWD", path, 1);
+					if(chdir(path) < 0)	
+						printError("cd");
+				} else {
+					strcpy(path, getcwd(0,0));
+					setenv("PWD", path, 1);
+				}
+			}else {
+				child_pid = fork();
+				if(child_pid == 0) {
+					// CHILD
+					parameters[0] = command;
+					for(i=1; i<6; i++) {
+						temp = strtok(NULL, " \n");
+						parameters[i] = temp;
+						
+					}
+					parameters[i] = (char*)NULL;
+					if(execvp(command, parameters) < 0) 
+						printError("execvp");
+					return -1;
+				} else {
+					
+					// PARENT
+					if( checkIfBG(input) ) {
+						// BG process
+
+					} else {
+						// FG process!
+						printf("Child created with PID: %i\n", child_pid);
+						waitpid(child_pid, NULL, 0);
+						printf("Child with PID: %i terminated\n", child_pid);
+					}
 					
 				}
-				parameters[i] = (char*)NULL;
-				if(execvp(command, parameters) < 0) 
-					printError("execvp");
-				return -1;
-			} else {
-				
-				// PARENT
-				if( checkIfBG(input) ) {
-					// BG process
-
-				} else {
-					// FG process!
-					printf("Child created with PID: %i\n", child_pid);
-					waitpid(child_pid, NULL, 0);
-					printf("Child with PID: %i terminated\n", child_pid);
-				}
-				
 			}
-		}
+
+		
 
 	}
 	// Free allocated memory
